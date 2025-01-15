d1745b55fdded1917f2b8ca00d0cf90dfd16af22
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-01-15 23:23:21 +0530

    socialhandles page done

--- src/components/SocialHandles.js
+++ src/components/SocialHandles.js
@@ -1,16 +1,31 @@
 import React from 'react';
-// import '../style/SocialHandles.css';
+import '../style/SocialHandles.css';
+import { FaGithub, FaLinkedin, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 function SocialHandles() {
   return (
     <div className="main-social">
-      <h2 className='heading'>Social Handles</h2>
-      {/* <ul>
-        <li>GitHub</li>
-        <li>Instagram</li>
-        <li>LinkedIn</li>
-        <li>YouTube</li>
-      </ul> */}
+      <h3 className='heading'> Connect with Me </h3>
+
+      <div className="social-links">
+
+        <a href="https://github.com/Vaibhavii3" target="_blank" rel="noopener noreferrer" className="social-link github">
+          <FaGithub />
+        </a>
+
+        <a href="https://linkedin.com/in/vaibhavi-chaudhari-1b003120b" target="_blank" rel="noopener noreferrer" className="social-link linkedin">
+          <FaLinkedin />
+        </a>
+
+        <a href="https://instagram.com/vaibhavii.3" target="_blank" rel="noopener noreferrer" className="social-link instagram">
+          <FaInstagram />
+        </a>
+
+        <a href="https://youtube.com/" target="_blank" rel="noopener noreferrer" className="social-link youtube">
+          <FaYoutube />
+        </a>
+
+      </div>
     </div>
   );
 }

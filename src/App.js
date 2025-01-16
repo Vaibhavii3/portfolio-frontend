5e424fbc87ff7b5107d5f238648198edf970e776
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-01-16 15:35:23 +0530

    animation updated

--- src/App.js
+++ src/App.js
@@ -1,69 +1,3 @@
-// import React from 'react';
-// import ProfileSection from './components/ProfileSection';
-// import SocialHandles from './components/SocialHandles';
-// import TechnicalSkills from './components/TechnicalSkills';
-// import Education from './components/Education';
-// import ProjectSection from './components/ProjectSection';
-// import Experience from './components/Experience';
-// import ContactSection from './components/ContactSection';
-// import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="app">
-
-//         <div className="grid-container">
-
-//           <div className="grid-item skills">
-//             <TechnicalSkills />
-//           </div>
-
-//           <div className="grid-item social">
-//             <SocialHandles />
-//           </div>
-
-//           <div className="grid-item experience">
-//             <Experience />
-//           </div>
-
-//           <div className="grid-item profile">
-//             <ProfileSection />
-//           </div>
-
-//           <div className="grid-item projects">
-//             <ProjectSection />
-//           </div>
-
-//           <div className="grid-item education">
-//             <Education />
-//           </div>
-
-//           <div className="grid-item contact">
-//             <ContactSection />
-//           </div>
-
-//         </div>
-
-//         <Routes>
-//           <Route path="/experience" element={<Experience />} />
-//           <Route path="/socialhandles" element={<SocialHandles />} />
-//           <Route path="/technicalSkills" element={<TechnicalSkills />} />
-//           <Route path="/education" element={<Education />} />
-//           <Route path="/projects" element={<ProjectSection />} />
-//           <Route path="/contact" element={<ContactSection />} />
-//           <Route path="/profilePage" element={<ProfileSection />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-// App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ProfileSection from './components/ProfileSection';
@@ -77,6 +11,7 @@ import './App.css';
 import { motion } from 'framer-motion';
 
 function App() {
+
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -88,12 +23,35 @@ function App() {
     },
   };
 
-  const itemVariants = {
-    hidden: { opacity: 0, scale: 0.8, y:50 },
+  const hoverEffect = {
+    scale: 1.02,
+    boxShadow: "0 8px 20px rgba(0, 0, 0, 0.3)", // Enhanced shadow
+    backgroundColor: "rgba(255, 255, 255, 0.3)", // Change background color on hover
+    transition: {
+      type: "spring",
+      stiffness: 150,
+      damping: 20,
+    },
+  };
+
+  const profileVariants = {
+    initial: { scale: 1, opacity: 1, x: 0, y: 0 },
+    animate: { scale: 0.8, opacity: 1,
+      transition: { 
+        type: "spring",
+        stiffness: 80,
+        damping: 12,
+        duration: 0.5 } },
+  };
+
+  // Specific directional variants for each section
+  const skillsVariants = {
+    hidden: { opacity: 0, scale: 0.8, y:100, x:100 }, // Skills coming from the left
     visible: { 
       opacity: 1, 
       scale: 1,
-      y: 0, 
+      y: 0,
+      x: 0,
       transition: { 
         type: "spring",
         stiffness: 80,
@@ -102,14 +60,74 @@ function App() {
     },
   };
 
-  const profileVariants = {
-    initial: { scale: 1, opacity: 1 },
-    animate: { scale: 0.8, opacity: 1,
+  const socialVariants = {
+    hidden: { opacity: 0, scale: 0.8, y: 100 }, // Social coming from the bottom
+    visible: { 
+      opacity: 1, 
+      scale: 1,
+      y: 0,
       transition: { 
         type: "spring",
         stiffness: 80,
         damping: 8,
-        duration: 0.8 } },
+      },
+    },
+  };
+
+  const experienceVariants = {
+    hidden: { opacity: 0, scale: 0.8, x: 100 }, // Experience coming from the right
+    visible: { 
+      opacity: 1, 
+      scale: 1,
+      x: 0,
+      transition: { 
+        type: "spring",
+        stiffness: 80,
+        damping: 8,
+      },
+    },
+  };
+
+  const projectVariants = {
+    hidden: { opacity: 0, scale: 0.8, x: -100 }, // project coming from the left
+    visible: { 
+      opacity: 1, 
+      scale: 1,
+      x: 0,
+      transition: { 
+        type: "spring",
+        stiffness: 80,
+        damping: 8,
+      },
+    },
+  };
+
+  const educationVariants = {
+    hidden: { opacity: 0, scale: 0.8, x: -100 }, // education coming from the left
+    visible: { 
+      opacity: 1, 
+      scale: 1,
+      x: 0,
+      transition: { 
+        type: "spring",
+        stiffness: 80,
+        damping: 8,
+      },
+    },
+  };
+
+  const contactVariants = {
+    hidden: { opacity: 0, scale: 0.8, y: -100 }, // Social coming from the bottom
+    visible: { 
+      opacity: 1, 
+      scale: 1,
+      y: 0,
+      transition: { 
+        type: "spring",
+        stiffness: 80,
+        damping: 8,
+      },
+    },
   };
 
   return (
@@ -120,32 +138,40 @@ function App() {
         initial="hidden"
         animate="visible"
       >
-        <motion.div className="grid-item skills" variants={itemVariants}>
+        <motion.div className="grid-item skills" variants={skillsVariants} whileHover={hoverEffect}>
           <TechnicalSkills />
         </motion.div>
-        <motion.div className="grid-item social" variants={itemVariants}>
+
+        <motion.div className="grid-item social" variants={socialVariants} whileHover={hoverEffect}>
           <SocialHandles />
         </motion.div>
-        <motion.div className="grid-item experience" variants={itemVariants}>
+
+        <motion.div className="grid-item experience" variants={experienceVariants} whileHover={hoverEffect}>
           <Experience />
         </motion.div>
+
         <motion.div
           className="grid-item profile"
           variants={profileVariants}
           initial="initial"
           animate="animate"
+          whileHover={hoverEffect}
         >
           <ProfileSection />
         </motion.div>
-        <motion.div className="grid-item projects" variants={itemVariants}>
+
+        <motion.div className="grid-item projects" variants={projectVariants} whileHover={hoverEffect}>
           <ProjectSection />
         </motion.div>
-        <motion.div className="grid-item education" variants={itemVariants}>
+
+        <motion.div className="grid-item education" variants={educationVariants} whileHover={hoverEffect}>
           <Education />
         </motion.div>
-        <motion.div className="grid-item contact" variants={itemVariants}>
+
+        <motion.div className="grid-item contact" variants={contactVariants} whileHover={hoverEffect}>
           <ContactSection />
         </motion.div>
+
       </motion.div>
 
       <Routes>
